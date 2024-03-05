04efb3f9e9b474fd4469d4f4176ba7b5f6a354f1
Matthew Hodgson <matthew@matrix.org>
2024-03-05 02:38:12 +0000

    avoid double-renders in strict mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,15 @@
-import { useEffect } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
+import { Component } from "react";
 import "./App.css";
 
-function App() {
+class App extends Component {
 
-    useEffect(() => {
-
-        (async () => {
-            await invoke("login", {
-                params: {
-                    homeserver: "https://matrix.org",
-                    user_name: "matthewtest",
-                    password: "",
-                }
-            });
-
-            const timeline_items = await invoke("subscribe_timeline", {
-                roomId: "!QQpfJfZvqxbCfeDgCj:matrix.org",
-            });
-
-            console.log(timeline_items);
-        })();
-
-    }, []);
-
-    return (
-        <div className="container">
-            <h1>aurora</h1>
-        </div>
-    );
+    render() {
+        return (
+            <div className="container">
+                <h1>aurora</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
