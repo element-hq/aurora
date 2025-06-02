bdb1aeeb3eeff2b8dc624585b5c3259e378f292e
David Langley <davidl@element.io>
2025-06-02 16:05:05 +0200

    Add code to load the wasm, login and pass the clients around

--- src/ClientStore.tsx
+++ src/ClientStore.tsx
@@ -1,90 +1,84 @@
-import { invoke } from "@tauri-apps/api/tauri";
 import TimelineStore from "./TimelineStore.tsx";
 import RoomListStore from "./RoomListStore.tsx";
 import { Mutex } from "async-mutex";
+import { ClientBuilder, ClientInterface } from "./index.web.ts";
 
 interface LoginParams {
-	username: string;
-	password: string;
-	server: string;
+  username: string;
+  password: string;
+  server: string;
 }
 
 export enum ClientState {
-	Unknown = 0,
-	LoggedIn = 1,
-	LoggedOut = 2,
+  Unknown,
+  LoggedIn,
+  LoggedOut,
 }
 
 class ClientStore {
-	timelineStores: Map<string, TimelineStore> = new Map();
-	roomListStore?: RoomListStore;
+  timelineStores: Map<String, TimelineStore> = new Map();
+  roomListStore?: RoomListStore;
+  client?: ClientInterface;
 
-	mutex: Mutex = new Mutex();
+  mutex: Mutex = new Mutex();
 
-	clientState: ClientState = ClientState.Unknown;
-	listeners: CallableFunction[] = [];
+  clientState: ClientState = ClientState.Unknown;
+  listeners: CallableFunction[] = [];
 
-	login = async ({ username, password, server }: LoginParams) => {
-		const release = await this.mutex.acquire();
+  login = async ({ username, password, server }: LoginParams) => {
+    let release = await this.mutex.acquire();
+    let client = await new ClientBuilder().homeserverUrl(server).build();
 
-		//await new Promise(r => setTimeout(r, 2000));
-		console.log("starting sdk...");
-		await invoke("reset");
-		try {
-			await invoke("login", {
-				params: {
-					user_name: username,
-					password: password,
-					homeserver: server,
-				},
-			});
+    console.log("starting sdk...");
+    try {
+      await client.login(username, password, "rust-sdk", undefined);
+      console.log("logged in...");
+      this.client = client;
+      this.clientState = ClientState.LoggedIn;
+    } catch (e) {
+      console.log("login failed", e);
+      this.clientState = ClientState.LoggedOut;
+    }
 
-			console.log("logged in...");
-			this.clientState = ClientState.LoggedIn;
-		} catch (e) {
-			console.log("login failed", e);
-			this.clientState = ClientState.LoggedOut;
-		}
+    this.emit();
+    release();
+  };
 
-		this.emit();
-		release();
-	};
+  getTimelineStore = async (roomId: string) => {
+    if (roomId === "") return;
+    let release = await this.mutex.acquire();
+    release();
+    let store = this.timelineStores.get(roomId);
+    if (!store) {
+      store = new TimelineStore(roomId);
+      this.timelineStores.set(roomId, store);
+    }
+    return store;
+  };
 
-	getTimelineStore = async (roomId: string) => {
-		if (roomId === "") return;
-		const release = await this.mutex.acquire();
-		release();
-		let store = this.timelineStores.get(roomId);
-		if (!store) {
-			store = new TimelineStore(roomId);
-			this.timelineStores.set(roomId, store);
-		}
-		return store;
-	};
+  getRoomListStore = async () => {
+    let release = await this.mutex.acquire();
+    release();
+    this.roomListStore ||= new RoomListStore(this.client!!);
+    return this.roomListStore;
+  };
 
-	getRoomListStore = async () => {
-		const release = await this.mutex.acquire();
-		release();
-		this.roomListStore ||= new RoomListStore();
-		return this.roomListStore;
-	};
+  subscribe = (listener: any) => {
+    this.listeners = [...this.listeners, listener];
+    return () => {
+      this.listeners = this.listeners.filter((l) => l !== listener);
+    };
+  };
 
-	subscribe = (listener: any) => {
-		this.listeners = [...this.listeners, listener];
-		return () => {
-			this.listeners = this.listeners.filter((l) => l !== listener);
-		};
-	};
+  getSnapshot = (): ClientState => {
+    return this.clientState;
+  };
 
-	getSnapshot = (): ClientState => {
-		return this.clientState;
-	};
-
-	emit = () => {
-		for (const listener of this.listeners) {
-			listener();
-		}
-	};
+  emit = () => {
+    for (let listener of this.listeners) {
+      listener();
+    }
+  };
 }
 
 export default ClientStore;
